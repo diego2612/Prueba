9d9d0daa03d1ee7ab01f102f96a8eda27e7c9400
Issac <issacatedra@gmail.com>
2020-02-03 20:50:25 -0600

    agregué una rama

---
+++ prueba/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba
+{
+    class Class1
+    {
+        private float algo;
+        private float Metodo()
+        {
+            Console.WriteLine("hola mundo");
+            return (float)5.2;
+        }
+    }
+}
